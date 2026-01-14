549ffb893c72809aeefdb134e47e313b14d74313
azzamt11 <dfeqhasavrymchaplctionnphysics@gmail.com>
2026-01-14 13:52:35 +0800

    building feature: main

--- app/main/cmd/main/wire_gen.go
+++ app/main/cmd/main/wire_gen.go
@@ -7,32 +7,33 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"yinni_backend/app/main/internal/biz"
-	"yinni_backend/app/main/internal/conf"
 	"yinni_backend/app/main/internal/data"
 	"yinni_backend/app/main/internal/server"
 	"yinni_backend/app/main/internal/service"
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
+	"yinni_backend/internal/conf"
 )
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	_ "go.uber.org/automaxprocs"
 )
 
 // Injectors from wire.go:
 
 // wireApp init kratos application.
-func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
+func wireApp(confServer *conf.Server, auth *conf.Auth, confData *conf.Data, logger log.Logger) (*kratos.App, func(), error) {
 	dataData, cleanup, err := data.NewData(confData)
 	if err != nil {
 		return nil, nil, err
 	}
-	greeterRepo := data.NewGreeterRepo(dataData, logger)
-	greeterUsecase := biz.NewGreeterUsecase(greeterRepo)
-	greeterService := service.NewGreeterService(greeterUsecase)
-	grpcServer := server.NewGRPCServer(confServer, greeterService, logger)
-	httpServer := server.NewHTTPServer(confServer, greeterService, logger)
+	mainRepo := data.NewMainRepo(dataData, logger)
+	mainUsecase := biz.NewMainUsecase(mainRepo)
+	mainService := service.NewMainService(mainUsecase)
+	grpcServer := server.NewGRPCServer(confServer, mainService, logger)
+	httpServer := server.NewHTTPServer(confServer, auth, mainService, logger)
 	app := newApp(logger, grpcServer, httpServer)
 	return app, func() {
 		cleanup()
